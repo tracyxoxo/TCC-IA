6653babb0abc4e1b3d894e920feea118872cb044
JP Borgneth <jp.borgneth@gmail.com>
2025-04-13 16:16:27 -0300

    added directory removal before extraction

--- utilities.py
+++ utilities.py
@@ -1,6 +1,6 @@
 import os
 import zipfile
-from shutil import move
+import shutil as sh 
 from typing import List
 
 def extract_filtered_files(
@@ -11,6 +11,13 @@ def extract_filtered_files(
     
     final_path = extract_path + "_final"
     os.makedirs(extract_path, exist_ok=True)
+
+    if os.path.exists(extract_path) and os.path.isdir(extract_path):
+        sh.rmtree(extract_path)
+
+    
+    if os.path.exists(final_path) and os.path.isdir(final_path):
+        sh.rmtree(final_path)
 
     for zip_file in os.listdir(files_path):
         if zip_file.endswith(".zip"):
@@ -24,13 +31,13 @@ def extract_filtered_files(
         full_name = os.path.join(extract_path, dir)
         if os.path.isdir(full_name) and os.listdir(full_name).count(dir) == 0:
             os.makedirs(final_path, exist_ok=True)
-            move(full_name, os.path.join(final_path, dir))
+            sh.move(full_name, os.path.join(final_path, dir))
 
     for dir in os.listdir(extract_path):
         full_name = os.path.join(extract_path, dir)
         if os.path.isdir(full_name) and os.listdir(full_name).count(dir) > 0:
             os.makedirs(final_path, exist_ok=True)
-            move(os.path.join(full_name, dir), final_path)
+            sh.move(os.path.join(full_name, dir), final_path)
 
     filterFiles_paths = []
     for root, dirs, files in os.walk(final_path):
